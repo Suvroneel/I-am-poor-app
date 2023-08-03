a6fb13853a35d4ef2ccac6a7eb58a7b5704e5207
Suvroneel Nathak <suvroneelnathak5832@gmail.com>
2023-08-03 12:25:44 +0530

    Basic flutter app

---
+++ i_am_poor_app/android/app/src/main/kotlin/suvroneelnathak/i_am_poor_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package suvroneelnathak.i_am_poor_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ i_am_poor_app/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: Center(child: Text('I Am Poor')
+        ),
+        backgroundColor: Colors.red,
+      ),
+    ),
+  ),
+  );
+}
